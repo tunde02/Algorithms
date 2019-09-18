fbbdc1c52980c99c5d71181f29d7f44ff89f6d14
SeongHo Jin <snskrktja@naver.com>
2019-09-18 17:28:25 +0900

    2019.09.18

--- 1012.cpp
+++ 1012.cpp
@@ -1,1 +1,76 @@
-// 지뢰찾기에서 했던 것처럼 비슷하게 하면 쉽게 풀 수 있을듯
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int t, r, c, cabbages;
+
+int calcWarms(vector<vector<int>> map);
+void erase(vector<vector<int>>* map, int x, int y);
+void print(vector<vector<int>> map);
+
+int main() {
+	cin >> t;
+
+	for (int tests = 0; tests < t; tests++) {
+		cin >> r >> c >> cabbages;
+		vector<vector<int>> map(r, vector<int>(c, 0));
+
+		for (int i = 0; i < cabbages; i++) {
+			int x, y;
+			cin >> x >> y;
+			map[x][y] = 1;
+		}
+		print(map);
+		cout << calcWarms(map) << "\n";
+	}
+}
+
+void initMap(vector<vector<int>> map) {
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			map[i][j] = 0;
+		}
+	}
+}
+
+int calcWarms(vector<vector<int>> map) {
+	int warms = 0;
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			if (map[i][j] == 1) {
+				erase(&map, i, j);
+				warms++;
+			}
+		}
+	}
+
+	return warms;
+}
+
+void erase(vector<vector<int>>* map, int x, int y) {
+	(*(map))[x][y] = 0;
+
+	if (x - 1 >= 0 && (*(map))[x - 1][y] == 1) {
+		erase(map, x - 1, y);
+	}
+	if (x + 1 < r && (*(map))[x + 1][y] == 1) {
+		erase(map, x + 1, y);
+	}
+	if (y - 1 >= 0 && (*(map))[x][y - 1] == 1) {
+		erase(map, x, y - 1);
+	}
+	if (y + 1 < c && (*(map))[x][y + 1] == 1) {
+		erase(map, x, y + 1);
+	}
+}
+
+void print(vector<vector<int>> map) {
+	cout << "===========================\n";
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cout << map[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
